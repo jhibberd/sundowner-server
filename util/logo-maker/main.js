edd4150d530651327eeb7cb4d06326921016c7ba
James Hibberd <james.d.hibberd@gmail.com>
2013-10-10 17:03:27 +0800

    updated logo

--- util/logo-maker/main.js
+++ util/logo-maker/main.js
@@ -31,53 +31,39 @@ function drawLogo(size, opaqueBackground) {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
 
-    // define circle center, radii and line width
-    var circleCenterX =     size / 2;
-    var circleCenterY =     size * .45;
-    var circleRadius =      size * .25;
-    var circleRadiusBig =   size * .37;
-    var lineWidth =         size * .05;
-
     // size the canvas
     ctx.canvas.width = size;
     ctx.canvas.height = size;
 
     // fill background
-    ctx.fillStyle = opaqueBackground ? '#ffffff' : 'rgba(0, 0, 0, 0)';
+    var grd = ctx.createLinearGradient(0, 0, 0, size);
+    grd.addColorStop(1, "#0099CC");
+    grd.addColorStop(0, "#33B5E5");
+    ctx.fillStyle = grd
     ctx.fillRect(0, 0, size, size);
 
-    // draw background column
-    ctx.fillStyle = '#669900';
+    // box
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(size * .15, size * .15, size * .7, size * .4);
+
+    // callout
     ctx.beginPath();
-    ctx.arc(
-        circleCenterX, 
-        circleCenterY, 
-        circleRadiusBig, 
-        1 * Math.PI, 
-        2 * Math.PI, 
-        false);
-    ctx.lineTo(circleCenterX + circleRadiusBig, size);
-    ctx.lineTo(circleCenterX - circleRadiusBig, size);
+    ctx.moveTo((size / 2) - (size * .09), size * .54);
+    ctx.lineTo((size / 2), size * .64);
+    ctx.lineTo((size / 2) + (size * .09), size * .54);
     ctx.fill();
 
-    // draw circle
-    ctx.fillStyle = '#99CC00';
+    // circle
+    ctx.fillStyle = "#99CC00";
     ctx.beginPath();
     ctx.arc(
-        circleCenterX, 
-        circleCenterY, 
-        circleRadius, 
+        size / 2, 
+        size * .78, 
+        size * .11, 
         0, 
         2 * Math.PI, 
         false);
     ctx.fill();
-
-    // draw vertical line
-    ctx.fillRect(
-        circleCenterX - (lineWidth /2),
-        circleCenterX + (lineWidth /2),
-        lineWidth,
-        size - circleCenterY);
 }
 
 function openLogo() {
