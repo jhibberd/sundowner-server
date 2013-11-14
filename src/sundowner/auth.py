c3c1038171d8a9647d457030c1894798b83ef50d
James Hibberd <james.d.hibberd@gmail.com>
2013-11-14 18:10:19 +0800

    first draft of api that delegates authorization to facebook

---
+++ src/sundowner/auth.py
@@ -0,0 +1,144 @@
+"""Authenticates requests (by delegating access token validation to Facebook).
+"""
+
+import httplib
+import json
+import redis
+import sundowner.config
+import sundowner.data
+import time
+import tornado.gen
+import tornado.httpclient
+import urllib
+from bson.objectid import ObjectId
+
+
+@tornado.gen.coroutine
+def validate(access_token):
+    """Validate a Facebook access token and return the associated local user
+    ID, creating a new user record if necessary.
+    """
+
+    user_id = _Cache.get(access_token)
+
+    # If the access token isn't in the cache then validate it with Facebook
+    # and retrieve its metadata. Extract the user ID from the metadata and 
+    # query Facebook again for the user metadata. If a record for the user
+    # already exists update it with the latest metadata, otherwise create a new
+    # record. Finally cache the access token until Facebook expires it.
+    if user_id is None:
+        access_token_meta = yield FacebookGraphAPI.debug_token(access_token)
+        fb_user_id = access_token_meta["user_id"]
+        user_meta = yield FacebookGraphAPI.get_user(fb_user_id, access_token)
+        user_id = yield _create_or_update_user_record(fb_user_id, user_meta)
+        _Cache.put(access_token, user_id, access_token_meta["expiry"])
+
+    raise tornado.gen.Return(user_id)
+
+@tornado.gen.coroutine
+def _create_or_upate_user_record(fb_user_id, user_meta):
+    user_record = \
+        yield sundowner.data.users.read_by_facebook_user_id(fb_user_id)
+    if user_record is None:
+        user_id = yield sundowner.data.users.create(user_meta)
+    else:
+        user_record["facebook"] = user_meta
+        yield sundowner.data.users.update(user_record)
+        user_id = user_record["_id"]
+    raise tornado.get.Return(user_id)
+
+class FacebookGraphAPI(object):
+
+    @staticmethod
+    @tornado.gen.coroutine
+    def debug_token(access_token):
+        """Retrieve access token metadata.
+        
+        https://developers.facebook.com/docs/facebook-login/access-tokens/
+        See "Getting Info about Tokens and Debugging"
+        """
+
+        app_access_token = "%s|%s" % (
+            sundowner.config.cfg["fb-app-id"],
+            sundowner.config.cfg["fb-app-secret"])
+        url = "https://graph.facebook.com/debug_token?" + urllib.urlencode({
+            "input_token":  access_token,
+            "access_token": app_access_token,
+            })
+
+        http_client = tornado.httpclient.AsyncHTTPClient()
+        try:
+            response = yield http_client.fetch(url)
+        except tornado.httpclient.HTTPError as e:
+            # Facebook will return an error if the access token doesn't match
+            # the app ID
+            response_body = json.loads(e.response.body)
+            msg = response_body["error"]["message"]
+            raise AuthError(msg)
+        else:
+            response_body = json.loads(response.body)
+
+        # TODO is it possible that 'is_valid' could be False?
+        data = response_body["data"]
+        if data["is_valid"] != True:
+            raise AuthError("Access token isn't valid")
+
+        raise tornado.gen.Return({
+            "user_id":      data["user_id"],
+            "expires_at":   data["expires_at"],
+            })
+
+    @staticmethod
+    @tornado.gen.coroutine
+    def get_user(user_id, access_token):
+        """Get metadata associated with a Facebook user ID.""" 
+        url = "https://graph.facebook.com/%s?%s" % (user_id, urllib.urlencode({
+            "access_token": access_token,
+            }))
+        http_client = tornado.httpclient.AsyncHTTPClient()
+        response = yield http_client.fetch(url)
+        user_meta = json.loads(response.body)
+        raise tornado.gen.Return(user_meta)
+
+
+class _Cache(object):
+    """Cache user IDs by access token."""
+
+    @classmethod
+    def get(cls, access_token):
+        key = cls._key(access_token)
+        user_id = cls._get_conn().get(key)
+        if user_id is None:
+            return None
+        else:
+            return ObjectId(user_id)
+
+    @classmethod
+    def put(cls, access_token, user_id, expiry):
+        key = cls._key(access_token)
+        cls._get_conn().set(
+            name=   key, 
+            value=  str(user_id),                   # from ObjectId
+            ex=     (expiry - long(time.time())     # from timestamp to secs
+            )
+
+    @classmethod
+    def _get_conn(cls):
+        if not hasattr(cls, "_conn"):
+            cls._conn = redis.Redis()
+        return cls._conn
+
+    @staticmethod
+    def _key(access_token):
+        return "%s/%s" % (sundowner.config.cfg["cache-key-auth"], access_token)
+
+
+class AuthError(Exception): pass
+
+if __name__ == "__main__":
+    sundowner.config.init("/home/jhibberd/projects/sundowner/cfg/sandbox.yaml")
+    access_token = "CAADETKZBOw4kBAAj5YlOaZB02G7fJx3yKAP2ZAdP6B9JqbbGoOXjG8K7zdc8xbvqVhFuc3nt7LG2K1pmZBgXTx2ZAFA0OWZCMkCJ0JlJ5W8qjjZBRhrveLBTMlJmTG1vVG3s1gsCnAU6MLoQ0IQZAK349yBByHbiSRcRZBoTJDWoZClgiZB8NCoJnYAeeu4XECZBt0oWUSyigbLd1tGRhTHwQZBtH"
+    data = FacebookGraphAPI.debug_token(access_token)
+    print FacebookGraphAPI.get_user(data["user_id"], access_token) 
+    #print FacebookGraphAPI.debug_token("CAACEdEose0cBADOSxMHr9S5pvwIdfyUcIlm72oMC8ZAMwAZAWnO5110nY0myLl1WAsZBNOeSZBGUSqfCTGQoz99mRVX66OhYth2olXPZCRZC4GrBNhBW5bReE1ZAbq0RbhlZBbDuROoiYtXF20X0EP2U6loJnADRWDTzXKDZBlem8o7m67nM3UctlI2jWiAbeXo4ZD")
+
--- src/sundowner/main.py
+++ src/sundowner/main.py
@@ -82,12 +82,12 @@ class ContentHandler(RequestHandler):
     def get(self):
         """Return top content near a location."""
 
+        user_id = yield auth.validate(self.get_argument("access_token"))
         args = {
             "lng":      self.get_argument("lng"),
             "lat":      self.get_argument("lat"),
-            "user_id":  self.get_argument("user_id"),
             }
-        yield validate.ContentHandlerValidator().validate_get(args)
+        validate.ContentHandlerValidator().validate_get(args)
 
         # get all nearby content
         top_content = yield sundowner.data.content.get_nearby(
@@ -114,7 +114,7 @@ class ContentHandler(RequestHandler):
 
         # write activity
         self.settings["activity_pub"].write_user_view_content(
-            args["user_id"], args["lng"], args["lat"])
+            user_id, args["lng"], args["lat"])
 
         self.complete(data=result)
 
@@ -122,23 +122,24 @@ class ContentHandler(RequestHandler):
     def post(self):
         """Save content to the database."""
 
+        user_id = yield auth.validate(self.get_argument("access_token"))
+
         payload =           self.get_json_request_body()
         args = {
             "lng":          payload.get("lng"),
             "lat":          payload.get("lat"),
             "text":         payload.get("text"),
-            "user_id":      payload.get("user_id"),
             "accuracy":     payload.get("accuracy"),
             "url":          payload.get("url"),
             }
-        yield validate.ContentHandlerValidator().validate_post(args)
+        validate.ContentHandlerValidator().validate_post(args)
 
         content_id = ObjectId()
         yield sundowner.data.content.put({
             "_id":              content_id, 
             "text":             args["text"],
             "url":              args["url"],
-            "user_id":          args["user_id"],
+            "user_id":          user_id,
             "accuracy":         args["accuracy"], # meters
             "loc": {
                 "type":         "Point",
@@ -158,7 +159,7 @@ class ContentHandler(RequestHandler):
 
         # write activity
         self.settings["activity_pub"].write_user_create_content(
-            args["user_id"], content_id)
+            user_id, content_id)
 
         self.complete(httplib.CREATED)
 
@@ -169,90 +170,32 @@ class VotesHandler(RequestHandler):
     def post(self):
         """Register a vote up or down against a piece of content."""
 
+        user_id = yield auth.validate(self.get_argument("access_token"))
+
         payload =           self.get_json_request_body()
         args = {
             "content_id":   payload.get("content_id"),
-            "user_id":      payload.get("user_id"),
             "vote":         payload.get("vote"),
             }
         yield validate.VotesHandlerValidator().validate_get(args)
 
         accepted = yield sundowner.data.votes.put(
-            args["user_id"], args["content_id"], args["vote"])
+            user_id, args["content_id"], args["vote"])
         if accepted:
             yield sundowner.data.content.inc_vote(
                 args["content_id"], args["vote"])
-            # otherwise the vote has already been places
+            # otherwise the vote has already been placed
 
         # write activity
         if args["vote"] == Vote.UP:
             self.settings["activity_pub"].write_user_like_content(
-                args["user_id"], args["content_id"])
+                user_id, args["content_id"])
         else: # already validated so only logical alternative
             self.settings["activity_pub"].write_user_dislike_content(
-                args["user_id"], args["content_id"])
+                user_id, args["content_id"])
 
         status_code = httplib.CREATED if accepted else httplib.OK
         self.complete(status_code)
-
-
-class UsersHandler(RequestHandler):
-
-    @tornado.gen.coroutine
-    def post(self):
-        """Resolve a Facebook access token to a user ID.
-        
-        If this is the first time that the system has encountered the Facebook
-        user ID associated with the access token then create a new user.
-        """
-
-        payload =           self.get_json_request_body()
-        args = {
-            "access_token": payload.get("access_token"),
-            }
-        validate.UsersHandlerValidator().validate_post(args)
-
-        # validate the access token using the Facebook Graph API and at the
-        # same time retrieve data on the user associated with it
-        http_client = tornado.httpclient.AsyncHTTPClient()
-        url = "https://graph.facebook.com/me?access_token=%s" % \
-            args["access_token"]
-        try:
-            response = yield http_client.fetch(url)
-        except tornado.httpclient.HTTPError as e:
-            fb_error = json.loads(e.response.body)
-            if fb_error["error"]["type"] == "OAuthException":
-                raise BadRequestError("'access_token' is not valid.")
-            else:
-                raise e
-        fb_response = json.loads(response.body)
-
-        # attempt to lookup the user in the "users" collection by their
-        # Facebook user ID
-        fb_user_id = fb_response["id"]
-        user_data = yield \
-            sundowner.data.users.get_by_facebook_id(fb_user_id)
-
-        # if no user is found created a new user using the user data retrieved
-        # from Facebook
-        if user_data is None:
-            result = yield \
-                sundowner.data.users.create_from_facebook_data(fb_response)
-            created = True
-
-        # if a user is found extract the native ID and Facebook name
-        else:
-            result = {
-                "id":       user_data["_id"],
-                "name":     user_data["facebook"]["name"],
-                }
-            created = False
-
-        # can't JSON encode ObjectId object
-        result["id"] = str(result["id"])
-
-        status_code = httplib.CREATED if created else httplib.OK
-        self.complete(status_code, data=result)
 
 
 # Main -------------------------------------------------------------------------
@@ -262,17 +205,16 @@ def main():
     try:
         config_filepath = sys.argv[1]
     except IndexError:
-        raise Exception('No config file specified')
+        raise Exception("No config file specified")
     sundowner.config.init(config_filepath)
     sundowner.data.connect()
 
     application = tornado.web.Application([
         (r"/content",   ContentHandler),    # GET, POST
         (r"/votes",     VotesHandler),      # POST
-        (r"/users",     UsersHandler),      # POST
         ], 
         activity_pub=ActivityPub())
-    application.listen(sundowner.config.cfg['port'])
+    application.listen(sundowner.config.cfg["port"])
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == '__main__':
