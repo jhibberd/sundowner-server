f33d6515523a31808e4f3c3bcd570c4fa07b3a41
James Hibberd <james.d.hibberd@gmail.com>
2013-11-18 16:53:24 +0800

    user page for analytics app

--- src/sundowner_analytics/db.py
+++ src/sundowner_analytics/db.py
@@ -1,50 +1,45 @@
 import datetime
 import motor
-import os.path
 import pymongo
 import sundowner.config
-import sys
 import tornado.gen
-import tornado.ioloop
-import tornado.web
 from sundowner.analytics.activity.pub import Verb
 
 
-class ActivityStore(object):
+class Database(object):
 
-    _BATCH_SIZE = 10
+    def __init__(self, conn):
+        self._analytics =   conn[sundowner.config.cfg["db-name-analytics"]]
+        self._primary =     conn[sundowner.config.cfg["db-name-primary"]]
 
-    @classmethod
+    _BATCH_SIZE = 100
     @tornado.gen.coroutine
-    def get(cls, db_conn):
-
-        db_analytics =  db_conn[sundowner.config.cfg["db-name-analytics"]]
-        db_primary =    db_conn[sundowner.config.cfg["db-name-primary"]]
+    def get_recent_activity(self):
 
         result = []
-        cursor = db_analytics.activity.find()
-        cursor.sort([("_id", pymongo.DESCENDING)]).limit(cls._BATCH_SIZE)
+        cursor = self._analytics.activity.find()
+        cursor.sort([("_id", pymongo.DESCENDING)]).limit(self._BATCH_SIZE)
         while (yield cursor.fetch_next):
             activity = cursor.next_object()
 
             # augment actor data
-            activity["actor_data"] = \
-                yield motor.Op(
-                    db_primary.users.find_one, {"_id": activity["actor"]}) 
+            activity["actor_data"] = yield self.get_user(activity["actor"])
 
             # augment subject data (if available)
             if activity["verb"] in [Verb.CREATE, Verb.LIKE, Verb.DISLIKE]:
                 activity["subject_data"] = \
                     yield motor.Op(
-                        db_primary.content.find_one,
+                        self._primary.content.find_one,
                         {"_id": activity["subject"]["content_id"]}) 
 
             # format time
-            time = cls._fmt_timestamp(activity["created_time"])
+            time = self._fmt_timestamp(activity["created_time"])
 
             # format actor
-            actor = "%s (%s)" % \
-                (activity["actor_data"]["facebook"]["name"], activity["actor"])
+            actor = {
+                "name": activity["actor_data"]["facebook"]["name"], 
+                "id":   activity["actor"],
+                }
 
             # format verb
             verb = activity["verb"] 
@@ -79,44 +74,13 @@ class ActivityStore(object):
 
         raise tornado.gen.Return(result)
 
+    @tornado.gen.coroutine
+    def get_user(self, user_id):
+        result = yield motor.Op(
+            self._primary.users.find_one, {"_id": user_id})
+        raise tornado.gen.Return(result)
+
     @staticmethod
     def _fmt_timestamp(ts):
         return datetime.datetime.fromtimestamp(ts).strftime("%H:%M:%S %d %b")
-
-
-class Handler(tornado.web.RequestHandler):
-
-    @tornado.web.asynchronous
-    @tornado.gen.coroutine
-    def get(self):
-        db_conn = self.settings["db_conn"]
-        activity = yield ActivityStore.get(db_conn)
-        self.render("activity.html", activity=activity)
-        self.finish()
-
-
-def main():
-
-    # init config
-    try:
-        config_filepath = sys.argv[1]
-    except IndexError:
-        raise Exception('No config file specified')
-    sundowner.config.init(config_filepath)
-
-    db_conn = motor.MotorClient(
-        sundowner.config.cfg["db-host"],
-        sundowner.config.cfg["db-port"],
-        ).open_sync()
-    application = tornado.web.Application([
-        (r"/", Handler),
-        ], 
-        template_path=os.path.join(os.path.dirname(__file__), "templates"),
-        db_conn=db_conn,
-        debug=True)
-    application.listen(sundowner.config.cfg["analytics-port"])
-    tornado.ioloop.IOLoop.instance().start()
-
-if __name__ == '__main__':
-    main()
 
---
+++ src/sundowner_analytics/main.py
@@ -0,0 +1,59 @@
+import datetime
+import motor
+import os.path
+import sundowner.config
+import sys
+import tornado.gen
+import tornado.ioloop
+import tornado.web
+from sundowner_analytics.db import Database
+from bson.objectid import ObjectId
+
+
+class ActivityHandler(tornado.web.RequestHandler):
+
+    @tornado.web.asynchronous
+    @tornado.gen.coroutine
+    def get(self):
+        db_conn = self.settings["db_conn"]
+        activity = yield Database(db_conn).get_recent_activity()
+        self.render("activity.html", activity=activity)
+
+
+class UsersHandler(tornado.web.RequestHandler):
+
+    @tornado.web.asynchronous
+    @tornado.gen.coroutine
+    def get(self, user_id):
+        user_id = ObjectId(user_id)
+        db_conn = self.settings["db_conn"]
+        user = yield Database(db_conn).get_user(user_id)
+        self.render("user.html", user=user)
+
+
+def main():
+
+    # init config
+    try:
+        config_filepath = sys.argv[1]
+    except IndexError:
+        raise Exception('No config file specified')
+    sundowner.config.init(config_filepath)
+
+    db_conn = motor.MotorClient(
+        sundowner.config.cfg["db-host"],
+        sundowner.config.cfg["db-port"],
+        ).open_sync()
+    application = tornado.web.Application([
+        (r"/",                          ActivityHandler),
+        (r"/users/([0-9a-f]{24})/?",    UsersHandler),
+        ], 
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        db_conn=db_conn,
+        debug=True)
+    application.listen(sundowner.config.cfg["analytics-port"])
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+    main()
+
